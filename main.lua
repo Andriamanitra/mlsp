84c8310481a7d5ff58a9f02b1358c5365b99d526
usfbih8u <>
2025-07-23 14:26:40 +0200

    Do not preserve the cursors' selections in `formatAction()`

--- main.lua
+++ main.lua
@@ -733,6 +733,8 @@ function formatAction(bufpane)
         end
     end
 
+    buf:DeselectCursors() -- do not preserve the selections
+
     client:request(req, {
         onResult = onResult,
         onError = defaultOnErrorHandler(method)
